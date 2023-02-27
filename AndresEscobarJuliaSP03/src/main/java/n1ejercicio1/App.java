096ad48b7dd5c264912435f25d8c0e68476e3614
andres escobar <andres.escobar.godella@gmail.com>
2023-02-28 00:21:09 +0100

    version 01

---
+++ AndresEscobarJuliaSP03/src/main/java/n1ejercicio1/App.java
@@ -0,0 +1,64 @@
+package n1ejercicio1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ArrayList<Month> listameses = new ArrayList();
+    	
+    	
+    	listameses.add(new  Month("Enero") );
+    	listameses.add(new  Month("Febrero") );
+    	listameses.add(new  Month("Marzo") );
+    	listameses.add(new  Month("Abril") );  
+    	listameses.add(new  Month("Mayo") );
+    	listameses.add(new  Month("Junio") );
+    	listameses.add(new  Month("Julio") );
+    	listameses.add(new  Month("Septienbre") );
+    	listameses.add(new  Month("Octubre") );
+    	listameses.add(new  Month("Novienbre") );
+      	listameses.add(new  Month("Novienbre") );  	
+    	 Month mes2 = new  Month("Dicienbre");
+       	listameses.add( mes2);    
+       	listameses.add( mes2);    
+       	
+       	
+   		System.out.println("**  Listamos los 11 meses añadidos ");
+   		
+    	for ( Month mes : listameses) {
+    		System.out.println(" mes " +  mes.getName());
+    	}
+    	
+        listameses.add( 7, new  Month("Agosto")   );
+        
+   		System.out.println("**  Listamos los 12 meses incluyendo Agosto en su posicion ");
+    	for ( Month mes : listameses) {
+    		System.out.println(" mes " +  mes.getName());
+    	}
+        // constructor
+        HashSet<Month> 	listamesesconjunto = new HashSet<>(listameses);
+ 
+ 
+   		System.out.println("**  Recorremos el conjunto con for  ");
+        for ( Month  mes  : listamesesconjunto ) {
+            System.out.println(mes.getName());
+        }
+        
+    
+        Iterator<Month> mesiterator = listamesesconjunto.iterator();
+        
+   		System.out.println("**  Recorremos el conjunto con iterator  ");
+        while (mesiterator.hasNext()){
+      	Month  mes = mesiterator.next();
+
+            System.out.println(" mes  " + mes.getName());
+
+        }
+        
+    }
+}
---
+++ AndresEscobarJuliaSP03/src/main/java/n1ejercicio1/Month.java
@@ -0,0 +1,15 @@
+package n1ejercicio1;
+
+public class Month {
+	String name = null;
+
+	public String getName() {
+		return name;
+	}
+
+	public Month(String mes) {
+		this.name = mes;
+	}
+		
+		
+}
